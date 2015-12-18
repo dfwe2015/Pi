c7e771401277765b695acc8c6c0e1b7b4dddbd68
gitrobike <497125399@qq.com>
2015-12-18 08:52:54 +0800

    测试一下Pycharm github VCS

--- pi.py
+++ pi.py
@@ -1,14 +1,18 @@
 # -*- coding: utf-8 -*-
 
-import os, sys, socket,commands
+import os, sys, socket, commands
 import pi_commands
+
 #使用socket获取本机IP地址
 hostname = socket.gethostname()
 IPinfo = socket.gethostbyname_ex(hostname)
-# LocalIP = IPinfo[2]
-#将所得列表转换为字符串
+print(IPinfo)
+#将所得元组中的列表转换为字符串
 LocalIP = ''.join(IPinfo[2])
-# print(LocalIP)
+print(LocalIP)
+#将字符串分割成序列（列表）
+LocalIpList = LocalIP.split('.')
+print(LocalIpList)
 
 # 如果操作系统为Windows系统，则更改活动代码页Active code page 为437 英文。936 （ANSI/OEM - 简体中文 GBK）。
 if os.name == 'nt':
