7996a6325062a33a30ffa67d497ab89cb0678af1
gitrobike <497125399@qq.com>
2016-01-14 09:42:14 +0800

    练习例子，ex43.py《来自 Percal 25 号行星的哥顿人》

--- get_ping.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import ping_ipv4
-import sys
-
-ping_ipv4.pingNode(alive=0, timeout=1, ipv6=0, number=sys.maxint, \
-             node='192.168.1.1', flood=0, size=8)
--- mystuff.py
+++
@@ -1,4 +0,0 @@
-def apple():
-    print("I AM APPLES!")
-
-tangerine = "Living refilection of a dream"
